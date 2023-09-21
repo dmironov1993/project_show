42791d2a03e0ab93fa4c3a72193ccdf336ee58b5
Denis Mironov <45696712+dmironov1993@users.noreply.github.com>
2023-09-21 10:04:30 -0500

    Update main.cpp

--- cpp_projects/bot/telegram/main.cpp
+++ cpp_projects/bot/telegram/main.cpp
@@ -2,7 +2,7 @@
 
 int main() {
     const std::string api_endpoint =
-        "https://api.telegram.org/bot5618873476:AAESVX1rOxfL6aPEuDTvZdLe4XT5u79kBAE";
+        "https://api.telegram.org/.....";
     Bot bot(api_endpoint);
     bot.Run();
 
