f810041b4e0d6eab4a8e2ba9ba0eabd1f282e837
Denis Mironov <45696712+dmironov1993@users.noreply.github.com>
2023-09-21 09:37:46 -0500

    Update format.go

--- go_projects/gitfame/cmd/gitfame/format.go
+++ go_projects/gitfame/cmd/gitfame/format.go
@@ -1,7 +1,5 @@
 package main
 
-// before running this function 'format' has already been validated by 'formatvalidator.go'
-
 type FormatterStrategy func(author Authors)
 
 var Strategies = map[string]FormatterStrategy{
